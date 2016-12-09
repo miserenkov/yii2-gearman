dbcb74b9870f6163bc74e55f4a0897a2aca81645
Misha Serenkov <mi.serenkov@gmail.com>
2016-12-09 12:39:14 +0200

    test job

---
+++ tests/_data/TestJob.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Misha Serenkov
+ * Email: mi.serenkov@gmail.com
+ * Date: 09.12.2016 12:34
+ */
+
+namespace data;
+
+use miserenkov\gearman\JobBase;
+
+class TestJob extends JobBase
+{
+    public function execute(\GearmanJob $job = null)
+    {
+        var_dump($job);
+    }
+}
--- tests/unit/ComponentTest.php
+++ tests/unit/ComponentTest.php
@@ -16,4 +16,37 @@ class ComponentTest extends Codeception\Test\Unit
     {
         $this->assertTrue(extension_loaded('gearman'));
     }
+
+    public function testGearmanComponent()
+    {
+        Yii::$app->set('gearman', [
+            'class' => 'miserenkov\gearman\Gearman',
+            'servers' => [
+                ['host' => '127.0.0.1', 'port' => 4730],
+            ],
+            'user' => 'www-data',
+            'jobs' => [
+                'testJob' => [
+                    'class' => 'data\TestJob'
+                ],
+            ]
+        ]);
+
+        Yii::$app->controllerMap['gearman'] = [
+            'class' => 'miserenkov\gearman\controllers\GearmanController',
+            'gearmanComponent' => 'gearman'
+        ];
+
+        for ($i = 0; $i < 10000; $i++) {
+            Yii::$app->gearman->getDispatcher()->background('testJob', new \miserenkov\gearman\JobWorkload([
+                'params' => [
+                    'data' => $i
+                ]
+            ]));
+        }
+
+        Yii::$app->runAction('gearman/start', [
+            'id' => 1,
+        ]);
+    }
 }
